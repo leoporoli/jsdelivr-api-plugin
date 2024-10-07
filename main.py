e01e544207ea564e68cc4f9a8403ded175ff3de9
Leandro Poroli <leandro.poroli@kurtosistech.com>
2024-10-07 10:50:16 -0300

    update the plugin.CreateFlow signature to support multiple pod specs comming from different services

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import copy
 
-def create_flow(service_spec, pod_spec, flow_uuid, api_key):
-    modified_pod_spec = copy.deepcopy(pod_spec)
+
+def create_flow(service_specs: list, pod_specs: list, flow_uuid, api_key):
+    modified_pod_specs = []
+
+    for pod_spec in pod_specs:
+        modified_pod_spec = copy.deepcopy(pod_spec)
+        containers = modified_pod_spec.get('containers', [])
+
+        # Check if there are containers
+        if containers:
+            container = containers[0]
+            # Add environment variables
+            env_vars = container.get('env', [])
+
+            for env in env_vars:
+                if env.get("name") == "JSDELIVRAPIKEY":
+                    env["value"] = api_key
+
+            container['env'] = env_vars
+
+            modified_pod_spec['containers'][0] = container
+
+        modified_pod_specs.append(modified_pod_spec)
 
     return {
-        "pod_spec": modified_pod_spec,
-        "config_map": {
-        }
+        "pod_specs": modified_pod_specs,
+        "config_map": {}
     }
+
 
 def delete_flow(config_map, flow_uuid):
     return
